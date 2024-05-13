776427b10b53feaef719cfc44715d6d9fb054acb
Kuba1507 <kuba.romanski@onet.com.pl>
2024-05-13 12:13:07 +0200

    created footer for mobile

--- src/js/script.js
+++ src/js/script.js
@@ -1,55 +1,65 @@
-const body = document.querySelector('body')
-const header = document.querySelector('.header')
-const burgerBtn = document.querySelector('.hamburger')
-const navigation = document.querySelector('.nav')
-const navList = document.querySelector('.nav__menu')
-const navItems = document.querySelectorAll('.nav__menu-item-link')
+const body = document.querySelector("body");
+const header = document.querySelector(".header");
+const burgerBtn = document.querySelector(".hamburger");
+const navigation = document.querySelector(".nav");
+const navList = document.querySelector(".nav__menu");
+const navItems = document.querySelectorAll(".nav__menu-item-link");
+const yearSpan = document.querySelector(".actual-year");
 
 const blockScroll = () => {
-	body.classList.toggle('block-scroll')
-}
+	body.classList.toggle("block-scroll");
+};
 
 const handleNav = () => {
-	burgerBtn.classList.toggle('is-active')
-	navigation.classList.toggle('show-nav')
+	burgerBtn.classList.toggle("is-active");
+	navigation.classList.toggle("show-nav");
 
-	handleNavItemsAnimation()
-	blockScroll()
-}
+	handleNavItemsAnimation();
+	blockScroll();
+};
 
 const closeNav = () => {
-	burgerBtn.classList.remove('is-active')
-	navigation.classList.remove('show-nav')
-	navigation.classList.remove('navigation-shadow')
-	body.classList.remove('block-scroll')
+	burgerBtn.classList.remove("is-active");
+	navigation.classList.remove("show-nav");
+	navigation.classList.remove("navigation-shadow");
+	body.classList.remove("block-scroll");
 
-	navItems.forEach(item => {
-		item.classList.remove('nav-items-animation')
-	})
-}
+	navItems.forEach((item) => {
+		item.classList.remove("nav-items-animation");
+	});
+};
 
 const handleNavItemsAnimation = () => {
-	let delayTime = 0
+	let delayTime = 0;
 
-	navItems.forEach(item => {
-		item.classList.toggle('nav-items-animation')
-		item.style.animationDelay = '.' + delayTime + 's'
-		delayTime++
-	})
+	navItems.forEach((item) => {
+		item.classList.toggle("nav-items-animation");
+		item.style.animationDelay = "." + delayTime + "s";
+		delayTime++;
+	});
 
-	navigation.classList.toggle('navigation-shadow')
-}
+	navigation.classList.toggle("navigation-shadow");
+};
 
 const handleHeaderShadow = () => {
 	if (window.scrollY >= 50) {
-		header.classList.add('header-shadow')
+		header.classList.add("header-shadow");
 	} else {
-		header.classList.remove('header-shadow')
+		header.classList.remove("header-shadow");
 	}
-}
+};
 
-burgerBtn.addEventListener('click', handleNav)
-window.addEventListener('scroll', handleHeaderShadow)
-navItems.forEach(item => {
-	item.addEventListener('click', closeNav)
-})
+const setYear = () => {
+	const currentYear = new Date().getFullYear();
+
+	if (yearSpan) {
+		yearSpan.textContent = currentYear;
+	}
+};
+
+burgerBtn.addEventListener("click", handleNav);
+window.addEventListener("scroll", handleHeaderShadow);
+navItems.forEach((item) => {
+	item.addEventListener("click", closeNav);
+});
+document.addEventListener("DOMContentLoaded", setYear);
